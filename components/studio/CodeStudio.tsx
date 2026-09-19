b8f5aa4faa7f07f70ba7b8f525d9be1dae6123fa
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-19 11:21:57 +0200

    studio: remove obsolete execution warning import

--- components/studio/CodeStudio.tsx
+++ components/studio/CodeStudio.tsx
@@ -3,7 +3,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import {
-  ExclamationTriangleIcon,
   InformationCircleIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline';
